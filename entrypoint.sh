68496bae1807f8987e7e811d9bb7c038b2dc8bb5
magic_rb <richard@brezak.sk>
2024-06-24 17:36:23 +0200

    Fix
    Signed-off-by: magic_rb <richard@brezak.sk>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gh release create -t ${INPUT_TITLE} "$(date +%Y%m%d%H%M%S)" --generate-notes
+gh release create -t "${INPUT_TITLE}" "$(date +%Y%m%d%H%M%S)" --generate-notes ${INPUT_WORKDIR}
